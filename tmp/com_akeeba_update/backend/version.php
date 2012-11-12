07bd11bb613162f82fbbf50d74570df93cb1c9b7
Jonathan Bennett <jbennett@ccistudios.com>
2012-11-12 15:21:41 -0500

    Removed temp garb

--- tmp/com_akeeba_update/backend/version.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Protect from unauthorized access
-defined('_JEXEC') or die('Restricted Access');
-
-define('AKEEBA_PRO', '0');
-define('AKEEBA_VERSION', '3.6.6.rc1');
-define('AKEEBA_DATE', '2012-09-27');
